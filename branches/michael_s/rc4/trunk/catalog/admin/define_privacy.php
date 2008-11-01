accfba24f1725d95860cb5092bdf69ad99e0152e
oscmax <oscmax@a4920382-5936-0410-8c60-f14f5803c0f4>
2008-11-01 15:38:46 +0000

    Constant Update for Define Mainpage content pages.

--- branches/michael_s/rc4/trunk/catalog/admin/define_privacy.php
+++ branches/michael_s/rc4/trunk/catalog/admin/define_privacy.php
@@ -36,7 +36,7 @@ $Id: define_privacy.php 14 2006-07-28 17:42:07Z user $
   require(DIR_FCKEDITOR . 'fckeditor.php');
 // This will cause it to look for 'conditions_content.php'
 
-  $HTTP_GET_VARS['filename'] = DEFINE_PRIVACY_CONTENT;
+  $HTTP_GET_VARS['filename'] = FILENAME_DEFINE_PRIVACY_CONTENT;
 
   switch ($HTTP_GET_VARS['action']) {
     case 'save':

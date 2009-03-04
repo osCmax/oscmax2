ee2841da5efc790915961011a97731de265dc54a
oscmax <oscmax@a4920382-5936-0410-8c60-f14f5803c0f4>
2009-03-04 05:26:19 +0000

    includes/classes/ updates - file code formatting only

--- branches/michael_s/rc4/trunk/catalog/includes/classes/order_total.php
+++ branches/michael_s/rc4/trunk/catalog/includes/classes/order_total.php
@@ -77,7 +77,8 @@ $Id: order_total.php 3 2006-05-27 04:59:07Z user $
 
       return $output_string;
     }
-// BOF: MOD - ICW ORDER TOTAL CREDIT CLASS/GV SYSTEM
+
+// BOF - MOD: CREDIT CLASS Gift Voucher Contribution
 //
 // This function is called in checkout payment after display of payment methods. It actually calls
 // two credit class functions.
